aef5f0852599eee02b15602a5395096d91c7c700
mirriam-akongo <jemmirriako@gmail.com>
2020-10-15 14:32:38 +0300

    edit the name of productsUrl

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from "./product";
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
-  private _productUrl = "../assets/products.json";
+  private _productsUrl = "../assets/products.json";
   constructor(private _http: Http) {}
 
   getAlbum(id: number): Observable<Album> {
@@ -19,7 +19,7 @@ export class ProductService {
 
   getProducts(): Observable<Product[]> {
     return this._http
-      .get(this._productUrl)
+      .get(this._productsUrl)
       .map((response) => <Product[]>response.json());
   }
 }
